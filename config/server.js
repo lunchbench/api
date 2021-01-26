bb0dac2f88d65409a02e60b03145044feb278a3c
Ed Mason <mynameisprivate3@gmail.com>
2021-01-26 20:52:26 +0000

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6e16aa03845578e2624a61a9629fd682'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0a2a4ef6-9e4b-4ac5-ad86-9986e84c2922'
+};
